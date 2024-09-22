4b0ec37ff830d46d7213e68a246d038128492fa0
corbuncul <agafivan@gmail.com>
2024-09-23 01:31:20 +0300

    add view for short link

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,27 +3,15 @@ from rest_framework import routers
 
 from api.views import (
     DownloadView, IngredientViewSet, TagViewSet,
-    RecipeViewSet  # , AddDeleteRecipeShoppingCartViewSet, AddDeleteRecipeToFavoriteViewSet
+    RecipeViewSet
 )
 router = routers.DefaultRouter()
 
 router.register('recipes', RecipeViewSet)
-# router.register(r'recipes/(?P<id>\d+)/shopping_cart', AddDeleteRecipeShoppingCartViewSet, basename='shopping_cart')
-# router.register(r'recipes/(?P<id>\d+)/favorite', AddDeleteRecipeToFavoriteViewSet, basename='favorite')
 router.register('tags', TagViewSet)
 router.register('ingredients', IngredientViewSet)
 
 urlpatterns = [
-    # path('recipes/<int:id>/shopping_cart/',
-    #      AddDeleteRecipeShoppingCartViewSet.as_view(
-    #          {'post': 'create', 'delete': 'destroy'}
-    #      ),
-    #      name='shopping_cart'),
-    # path('recipes/<int:id>/favorite/',
-    #      AddDeleteRecipeToFavoriteViewSet.as_view(
-    #          {'post': 'create', 'delete': 'destroy'}
-    #      ),
-    #      name='favorite'),
     path('recipes/download_shopping_cart/', DownloadView.as_view(
         {'get': 'download'})
     ),
